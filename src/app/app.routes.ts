03f68b99b2509289688cb775e28072020cf2e23f
Steven Nagle <suppressed@avoidingbots.com>
2025-07-20 10:48:07 -0500

    continue to troubleshoot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ExperienceComponent } from './experience/experience';
+import { ContactComponent } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AboutMeComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'contact', component: ContactComponent }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes'; // or wherever your routes are defined
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+});
