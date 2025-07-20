fb70f097a1acf47106a66cd0deb0fedecba6b109
Steven Nagle <suppressed@avoidingbots.com>
2025-07-20 13:12:25 -0500

    updating /docs for display

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -25,11 +25,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 649, hash: 'ddada7481585854424af2ffe9fc3571072bc3e5352e0c94292c566de40836caf', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 954, hash: '33b2a36420743940aa3403bd8c34644749c21929de8a466f8fa28e2561731ef6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 21875, hash: '4fc7646c7d1c1c876bb8d5d0711265a313c6263ed41b00c9390f459c57fca66d', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'experience/index.html': {size: 22160, hash: '791b9b6820c7e09f4f300cabe03bc9002769d3b30d221695c0df24ea9bda9df7', text: () => import('./assets-chunks/experience_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 22083, hash: 'b01072c0a5ad43426cfef8f1f766e3237031297204709c9563014a6d4a1f19de', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 640, hash: '77ae381b5e7c838d6cf50714f2666828b7fe464f3020f0305d87d84946e306c5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 945, hash: 'befc6be95ec1adf862371e498fb61b8ee5442f11b3be49b118ac6b932284cb8f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'experience/index.html': {size: 22151, hash: '991fadffb89730ab7a62e18ce4611488a39e964c118c5d2f01043efd91c594dc', text: () => import('./assets-chunks/experience_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 21866, hash: '38ad2c9aa8bfc6798c906dbfee40a86d33a871e39b22c966ae5d412f36bef0f9', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 23712, hash: '08bc29f00edd70ff803e072ee074fceb155941cc91acf3cfc982d3e6c446342d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-PVVONERT.css': {size: 849, hash: 'vowDjKZU4gA', text: () => import('./assets-chunks/styles-PVVONERT_css.mjs').then(m => m.default)}
   },
 };
--- docs/server/assets-chunks/index_csr_html.mjs
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -2,13 +2,13 @@ export default `<!doctype html>
 <html lang="en" data-beasties-container="">
 <head>
   <meta charset="utf-8">
-  <title>AboutMePortfolio</title>
+  <title>Meet Me</title>
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <style>body{margin:0;font-family:Segoe UI,sans-serif;background-color:#121212;color:#fff}</style><link rel="stylesheet" href="styles-PVVONERT.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-PVVONERT.css"></noscript></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="main-NDTMI3JJ.js" type="module"></script></body>
+<script src="main-N5JJXZNE.js" type="module"></script></body>
 </html>
 `;
