26f584965cd6c580630bc5cffe075d0f988e11da
Steven Nagle <suppressed@avoidingbots.com>
2025-07-22 13:31:27 -0500

    updated copy

--- docs/chunk-22O4NHXP.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-7BUNKQ4M.js";import"./chunk-YBHER2BD.js";export{a as AboutMeComponent};
---
+++ docs/chunk-4ZO6ZJ7X.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-YIRHAXS3.js";import"./chunk-YBHER2BD.js";export{a as AboutMeComponent};
