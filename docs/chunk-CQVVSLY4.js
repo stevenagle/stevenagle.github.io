a90ec2185badef518a1e14a9b2958edce23fcfc8
Steven Nagle <suppressed@avoidingbots.com>
2025-07-21 19:48:41 -0500

    add navbar styling, about-me copy

---
+++ docs/chunk-CQVVSLY4.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-L6VYKIFB.js";import"./chunk-MLTNJKN7.js";export{a as AboutMeComponent};
--- docs/chunk-UCDYFCCN.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-C6DNT7MD.js";import"./chunk-MLTNJKN7.js";export{a as AboutMeComponent};
