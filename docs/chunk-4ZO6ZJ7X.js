a50ed093e592c2dab32e3d7c843098d351cd2a80
Steven Nagle <suppressed@avoidingbots.com>
2025-07-24 03:45:07 -0500

    make a minor copy upate

--- docs/chunk-4ZO6ZJ7X.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-YIRHAXS3.js";import"./chunk-YBHER2BD.js";export{a as AboutMeComponent};
---
+++ docs/chunk-BJCGHEGQ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-VTAXQ57D.js";import"./chunk-YBHER2BD.js";export{a as AboutMeComponent};
