5c385e4463a7b421572cfd7c4e9b1bafab011d2f
Steven Nagle <suppressed@avoidingbots.com>
2025-08-25 09:20:33 -0500

    text styling change on About Me

--- docs/chunk-AILOWTE4.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-LB4RYPE5.js";import"./chunk-YBHER2BD.js";export{a as AboutMeComponent};
---
+++ docs/chunk-MSD4KAOR.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-EKUXJ2NQ.js";import"./chunk-YBHER2BD.js";export{a as AboutMeComponent};
